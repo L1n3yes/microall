33ec21a6bb87b5f66dae6c7a067028bba6c62051
L1n3yes <140054386+L1n3yes@users.noreply.github.com>
2023-08-01 18:41:49 +0700

    removed serial

--- main.ts
+++ main.ts
@@ -3,10 +3,9 @@ enum RadioMessage {
     started = 53023
 }
 input.onPinPressed(TouchPin.P0, function () {
-    serial.writeLine("P0")
+	
 })
 input.onButtonPressed(Button.A, function () {
-    serial.writeLine("Apressed")
     HAND = randint(1, 3)
     if (HAND == 1) {
         basic.showLeds(`
@@ -17,7 +16,6 @@ input.onButtonPressed(Button.A, function () {
             # # # # #
             `)
         basic.clearScreen()
-        serial.writeLine("RPSPAPER")
     } else if (HAND == 2) {
         basic.showLeds(`
             . . . . .
@@ -27,7 +25,6 @@ input.onButtonPressed(Button.A, function () {
             . . . . .
             `)
         basic.clearScreen()
-        serial.writeLine("RPSROCK")
     } else {
         basic.showLeds(`
             # # . . #
@@ -37,25 +34,25 @@ input.onButtonPressed(Button.A, function () {
             # # . . #
             `)
         basic.clearScreen()
-        serial.writeLine("RPSSISSOR")
     }
 })
 input.onPinPressed(TouchPin.P2, function () {
-    serial.writeLine("P2")
+	
 })
 input.onButtonPressed(Button.B, function () {
-    serial.writeLine("Bpressed")
+	
 })
 input.onPinPressed(TouchPin.P1, function () {
-    serial.writeLine("P1")
+	
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    serial.writeLine("LOGOpressed")
     music.play(music.tonePlayable(randint(131, 988), music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
 })
+let runtime = 0
 let HAND = 0
 music.setBuiltInSpeakerEnabled(true)
 power.lowPowerEnable(LowPowerEnable.Prevent)
+music.play(music.createSoundExpression(WaveShape.Square, 1, 5000, 255, 255, 2000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
 basic.showLeds(`
     # # . . .
     # . . . .
@@ -106,8 +103,19 @@ basic.showLeds(`
     # # # # #
     `)
 basic.clearScreen()
-serial.writeLine("test")
-serial.writeLine("m:b active")
+loops.everyInterval(1000, function () {
+    runtime += 1
+})
+// nonstop stuff
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    WaitUntilBlocks.waitUntilButtonPressed(Button.AB)
+    WaitUntilBlocks.waitUntilPinPressed(TouchPin.P0)
+    WaitUntilBlocks.waitUntilPinPressed(TouchPin.P1)
+    music.play(music.stringPlayable("C C5 B D G A C - ", 120), music.PlaybackMode.UntilDone)
+})
 basic.forever(function () {
     WaitUntilBlocks.waitUntilButtonPressed(Button.B)
     basic.showLeds(`
@@ -155,22 +163,10 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (input.temperature() < 0) {
-        serial.writeLine("micro:bit cold")
         music.play(music.stringPlayable("C C C C C C C C ", 200), music.PlaybackMode.UntilDone)
     } else if (input.temperature() > 40) {
-        serial.writeLine("micro:bit hot")
         music.play(music.stringPlayable("C5 C5 C5 C5 C5 C5 C5 C5 ", 200), music.PlaybackMode.UntilDone)
     } else {
     	
     }
-})
-basic.forever(function () {
-    WaitUntilBlocks.waitUntilButtonPressed(Button.AB)
-    WaitUntilBlocks.waitUntilPinPressed(TouchPin.P0)
-    WaitUntilBlocks.waitUntilPinPressed(TouchPin.P1)
-    music.play(music.stringPlayable("C C5 B D G A C - ", 120), music.PlaybackMode.UntilDone)
-})
-// nonstop stuff
-basic.forever(function () {
-    serial.redirectToUSB()
 })
