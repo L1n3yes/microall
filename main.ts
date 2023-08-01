cddbabeabea3b3374ed6c49194ab8020c40d81b2
L1n3yes <140054386+L1n3yes@users.noreply.github.com>
2023-08-01 09:05:19 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+enum RadioMessage {
+    message1 = 49434,
+    started = 53023
+}
 input.onPinPressed(TouchPin.P0, function () {
     serial.writeLine("P0")
 })
@@ -6,9 +10,9 @@ input.onButtonPressed(Button.A, function () {
     HAND = randint(1, 3)
     if (HAND == 1) {
         basic.showLeds(`
-            . . . # #
             . . # # #
             . # # # #
+            # # # # #
             # # # # #
             # # # # #
             `)
@@ -148,16 +152,14 @@ basic.forever(function () {
         . . . . .
         . . . . .
         `)
-    basic.clearScreen()
 })
 basic.forever(function () {
-    serial.redirectToUSB()
     if (input.temperature() < 0) {
         serial.writeLine("micro:bit cold")
-        music.play(music.stringPlayable("C C5 C C5 C C5 - - ", 200), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("C C C C C C C C ", 200), music.PlaybackMode.UntilDone)
     } else if (input.temperature() > 40) {
         serial.writeLine("micro:bit hot")
-        music.play(music.stringPlayable("C5 C C5 C C5 C - - ", 200), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("C5 C5 C5 C5 C5 C5 C5 C5 ", 200), music.PlaybackMode.UntilDone)
     } else {
     	
     }
@@ -167,4 +169,8 @@ basic.forever(function () {
     WaitUntilBlocks.waitUntilPinPressed(TouchPin.P0)
     WaitUntilBlocks.waitUntilPinPressed(TouchPin.P1)
     music.play(music.stringPlayable("C C5 B D G A C - ", 120), music.PlaybackMode.UntilDone)
+})
+// nonstop stuff
+basic.forever(function () {
+    serial.redirectToUSB()
 })
