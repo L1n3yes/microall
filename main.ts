6ef5bad5027b3a0bb0a0630fcfda2362ee28d91e
L1n3yes <140054386+L1n3yes@users.noreply.github.com>
2024-02-17 11:59:31 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -95,9 +95,13 @@ input.onButtonPressed(Button.B, function () {
 input.onPinPressed(TouchPin.P1, function () {
 	
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.play(music.stringPlayable("G C G C5 G G C G ", 341), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("G C G C5 G G C - ", 341), music.PlaybackMode.UntilDone)
+})
 music.setBuiltInSpeakerEnabled(true)
 power.lowPowerEnable(LowPowerEnable.Prevent)
-music.play(music.createSoundExpression(WaveShape.Triangle, 1, 3936, 37, 37, 2000, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+music.play(music.createSoundExpression(WaveShape.Sine, 1, 5000, 255, 255, 2000, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
 basic.showLeds(`
     # # . . .
     # . . . .
@@ -151,10 +155,10 @@ basic.clearScreen()
 serial.writeLine("start")
 basic.forever(function () {
     if (input.temperature() < 0) {
-        music.play(music.stringPlayable("C C C C C C C C ", 200), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("C C5 C C5 C C5 C C5 ", 200), music.PlaybackMode.UntilDone)
         serial.writeLine("temp HI")
     } else if (input.temperature() > 40) {
-        music.play(music.stringPlayable("C5 C5 C5 C5 C5 C5 C5 C5 ", 200), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("C5 C C5 C C5 C C5 C ", 200), music.PlaybackMode.UntilDone)
         serial.writeLine("temp LO")
     } else {
     	
